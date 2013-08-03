4c5dd1a562d5d65dea35652bbbaad0ef00210f10
abelobog <abelobog@hotmail.com>
2013-08-03 13:20:09 -0500

    hotel2

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -6,9 +6,11 @@ $(document).ready(function(){
 			var nom = $('#regNom').val();
 			var mail = $('#regEmail').val();
 			var tel = $('#regTel').val();
+			var foto = $('#regFoto').attr('rel');
 			
-			if(nom != '' && mail != '' && tel != ''){
-				navigator.notification.alert(nom+'\n'+mail+'\n'+tel, null, 'Hotel','Bienvenido');
+			if(nom != '' && mail != '' && tel != '' && foto != undefined && foto != ''){
+				//navigator.notification.alert(nom+'\n'+mail+'\n'+tel, null, 'Hotel','Bienvenido');
+				subirDatos(nom,mail,tel,foto);
 			}else{
 				navigator.notification.alert('Todos los campos son requeridos', null, 'Registro','Aceptar');
 			}
